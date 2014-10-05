467ffaad1beea643d03145fe7697ea8a93ad27f6
BryceMehring <brycemehring@gmail.com>
2014-10-05 00:54:54 -0500

    Moved mainMenu into scenes/

--- source/main.lua
+++ source/main.lua
@@ -1,9 +1,10 @@
 
 --require('mobdebug').on()
 -- import
+
 flower = require "libraries/flower"
 config = require "config"
 flower.openWindow("Hexel", 800, 600)
 
 math.randomseed(os.clock())
-flower.openScene("mainMenu")
+flower.openScene("scenes/mainMenu")
