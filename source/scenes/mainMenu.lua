bdfdc5c40b7c9ebf57c157949313ccff8b9c8d7c
BryceMehring <brycemehring@gmail.com>
2014-10-10 23:55:49 -0500

    Converted main menu to use flower widgets.

--- source/scenes/mainMenu.lua
+++ source/scenes/mainMenu.lua
@@ -4,8 +4,9 @@ module(..., package.seeall)
 -- Constraints
 --------------------------------------------------------------------------------
 
-MENU_ITEMS = require "scenes/sceneList"
-ITEM_HEIGHT = 32
+local MENU_ITEMS = require "scenes/sceneList"
+local ITEM_WIDTH = flower.viewWidth / 2
+local ITEM_HEIGHT = 60
 
 --------------------------------------------------------------------------------
 -- Variables
@@ -18,6 +19,7 @@ local backButton = nil
 -- Functions
 --------------------------------------------------------------------------------
 
+-- TODO: convert this to use the new widget library
 function createBackButton(childScene)
     local layer = flower.Layer()
     layer:setTouchEnabled(true)
@@ -36,55 +38,30 @@ function createBackButton(childScene)
     backButton:addEventListener("touchDown", backButton_onTouchDown)
 end
 
+-- Populates the main menu with buttons for their corresponding state.
 function createMenuList()
     menuList = {}
-    local itemWidth = flower.viewWidth - 20
-    local itemHeight = ITEM_HEIGHT
     
-    for i, item in ipairs(MENU_ITEMS) do
-        local menuItem = createMenuItem(item, itemWidth, itemHeight)
-        menuItem:setPos(10, i * 40)
-        table.insert(menuList, menuItem)
-    end
-end
-
-function createMenuItem(item, itemWidth, itemHeight)
-    local rect = flower.Rect(itemWidth, itemHeight)
-    rect:setColor(0, 0, 0.5, 1)
-
-    local label = flower.Label(item.title, itemWidth, itemHeight)
-    if not item.scene then
-        label:setColor(0.35, 0.35, 0.35, 1)
-    end
-
-    local menuItem = flower.Group(layer, itemWidth, itemHeight)
-    menuItem.data = item
-    menuItem:addChild(rect)
-    menuItem:addChild(label)
-    menuItem:addEventListener("touchDown", menuItem_onTouchDown)
-    
-    function menuItem:setSize(width, height)
-        flower.Group.setSize(self, width, height)
-        for i, child in ipairs(self.children) do
-            child:setSize(width, height)
+    local function onClickCallback(item)
+        if item.scene then
+            local childScene = flower.openScene(item.scene, {animation = item.openAnime, params = item.params})
+            if childScene then
+                selectedData = item
+                createBackButton(childScene)
+            end
         end
     end
     
-    return menuItem    
-end
-
-function resizeMenuList()
-    local itemWidth = flower.viewWidth - 20
-    local itemHeight = ITEM_HEIGHT
-    
-    for i, menuItem in ipairs(menuList) do
-        menuItem:setSize(itemWidth, itemHeight)
-    end
-end
-
-function resetPosition()
-    if backButton then
-       backButton:setPos(flower.viewWidth - 100, 0)
+    for i, item in ipairs(MENU_ITEMS) do
+        local menuItem = widget.Button {
+            size = {ITEM_WIDTH, ITEM_HEIGHT},
+            text = item.title,
+            onClick = function()
+                onClickCallback(item)
+            end,
+            enabled = item.scene ~= nil,
+        }
+        view:addChild(menuItem)
     end
 end
 
@@ -97,47 +74,30 @@ function onCreate(e)
     layer:setTouchEnabled(true)
     scene:addChild(layer)
     
-    createMenuList()
-    
-    flower.Runtime:addEventListener("resize", onResize)
-    
     view = widget.UIView {
-        scene = scene
+        scene = scene,
+        layout = widget.BoxLayout {
+            gap = {5, 5},
+            align = {"center", "center"},
+        },
     }
     
-    button1 = widget.Button {
-        size = {200, 50},
-        pos = {10, 300},
-        text = "Test1",
-        parent = view,
+    createMenuList()
+    
+    -- TODO: have the quit button actually quit the game.
+    local quitButton = widget.Button {
+        size = {ITEM_WIDTH, ITEM_HEIGHT},
+        text = "Quit",
         onClick = nil,
         onDown = nil,
         onUp = nil,
+        enabled = false,
     }
+    
+    view:addChild(quitButton)
 end
 
 function onStart(e)
-end
-
-function onResize(e)
-    resizeMenuList()
-    resetPosition()
-end
-
-function menuItem_onTouchDown(e)
-    if flower.SceneMgr.transitioning then
-        return
-    end
-
-    local t = e.target
-    local data = t and t.data
-    if data and data.scene then
-        local childScene = flower.openScene(data.scene, {animation = data.openAnime, params = data.params})
-        if childScene then
-            selectedData = data
-            createBackButton(childScene)
-        end
-    end
 end
 
 function backButton_onTouchDown(e)
