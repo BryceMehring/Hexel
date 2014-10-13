e85d685fa4801b956b3a3b90a6d8f60b11bf212c
BryceMehring <brycemehring@gmail.com>
2014-10-12 19:29:18 -0500

    Added function to enable debugging.

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
-
 local _oldRequire = require
 require = function(path, ...)
     return _oldRequire("source." .. path:gsub("%/", "."), ...)
+end
+
+-- Call this to enable debugging, kind of a hack just for right now
+-- TODO: fix hack
+function enableDebugging()
+    _oldRequire('mobdebug').on()
 end
 
 require "main"
