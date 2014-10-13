3e8e79df81b2b5c50c6920257913206e9699b68a
Luke Allen <nightwolf55la@gmail.com>
2014-10-12 20:39:03 -0500

    Added map select page

---
+++ source/mapSelect.lua
@@ -0,0 +1,88 @@
+module(..., package.seeall)
+
+MAP_LIST = require "maps/mapList"
+ITEM_HEIGHT = 32
+
+
+function createMenuList()
+    menuList = {}
+    local itemWidth = flower.viewWidth - 20
+    local itemHeight = ITEM_HEIGHT
+    
+    for i, item in ipairs(MAP_LIST) do
+        local menuItem = createMenuItem(item, itemWidth, itemHeight)
+        menuItem:setPos(10, i * 40)
+        table.insert(menuList, menuItem)
+    end
+end
+
+function createMenuItem(item, itemWidth, itemHeight)
+    local rect = flower.Rect(itemWidth, itemHeight)
+    rect:setColor(0, 0, 0.5, 1)
+
+    local label = flower.Label(item.title, itemWidth, itemHeight)
+
+    local menuItem = flower.Group(layer, itemWidth, itemHeight)
+    menuItem.data = item
+    menuItem:addChild(rect)
+    menuItem:addChild(label)
+    menuItem:addEventListener("touchDown", menuItem_onTouchDown)
+    
+    function menuItem:setSize(width, height)
+        flower.Group.setSize(self, width, height)
+        for i, child in ipairs(self.children) do
+            child:setSize(width, height)
+        end
+    end
+    
+    return menuItem    
+end
+
+function resizeMenuList()
+    local itemWidth = flower.viewWidth - 20
+    local itemHeight = ITEM_HEIGHT
+    
+    for i, menuItem in ipairs(menuList) do
+        menuItem:setSize(itemWidth, itemHeight)
+    end
+end
+
+--------------------------------------------------------------------------------
+-- Event Handler
+--------------------------------------------------------------------------------
+
+function onCreate(e)
+    layer = flower.Layer()
+    layer:setTouchEnabled(true)
+    scene:addChild(layer)
+    
+    createMenuList()
+    
+    flower.Runtime:addEventListener("resize", onResize)
+end
+
+function onStart(e)
+    -- debug
+    MOAISim.forceGarbageCollection()
+    MOAISim.reportHistogram()
+
+end
+
+function onResize(e)
+    resizeMenuList()
+end
+
+function menuItem_onTouchDown(e)
+    if flower.SceneMgr.transitioning then
+        return
+    end
+
+    local t = e.target
+    local data = t and t.data
+    if data and data.path then
+        local input = assert(io.input(data.path))
+        local dataz = input:read("*a")
+        input:close()
+        print(dataz)
+    end
+end
--- source/scenes/sceneList.lua
+++ source/scenes/sceneList.lua
@@ -1,7 +1,7 @@
 -- TODO: update this to point to the correct scenes
 
 return {
-    {title = "Single Player",    scene = nil,                           openAnime = "fade", closeAnime = "fade"},
+    {title = "Single Player",    scene = 'mapSelect',                   openAnime = "fade", closeAnime = "fade"},
     {title = "Co-op",            scene = nil,                           openAnime = "fade", closeAnime = "fade"},
     {title = "Versus",           scene = nil,                           openAnime = "fade", closeAnime = "fade"},
     {title = "Map Editor",       scene = "scenes/mapEditor/mapEditor",  openAnime = "fade", closeAnime = "fade"},
