39ddf6efcf988321c463bef1617f8f1d8f50d5c1
BryceMehring <brycemehring@gmail.com>
2014-10-30 01:44:27 -0500

    Cleanup of bd989765c312957b3b64f56f82066bcba994f54a.

--- source/scenes/singlePlayer/tower.lua
+++ source/scenes/singlePlayer/tower.lua
@@ -1,10 +1,7 @@
-local flower = require "source/libraries/flower"
 local class = flower.class
-
 local Towers = require "assets/towers"
-
--- module
-local M = {}
+require "source/gridNeighbors"
+require "source/utilities/vector"
 
 Tower = class()
 
@@ -45,42 +42,13 @@ end
 function Tower:calculate_targets()
     self.targets[Tower.serialize_pos(self.pos)] = self.pos
     for i=1,self.range do
-        temp_targets = {}
-        flower.table.deepCopy(self.targets, temp_targets)
+        local temp_targets = flower.table.copy(self.targets)
         for key, pos in pairs(temp_targets) do
-            local temp_pos = {}
-            
-            temp_pos[1] = pos[1]
-            temp_pos[2] = pos[2] + 1
-            self:add_target(temp_pos)
-            
-            temp_pos[1] = pos[1]
-            temp_pos[2] = pos[2] + 2
-            self:add_target(temp_pos)
-            
-            temp_pos[1] = pos[1]
-            temp_pos[2] = pos[2] - 1
-            self:add_target(temp_pos)
-            
-            temp_pos[1] = pos[1]
-            temp_pos[2] = pos[2] - 2
-            self:add_target(temp_pos)
-            
-            if pos[2]%2 == 0 then
-                temp_pos[1] = pos[1] + 1
-            else
-                temp_pos[1] = pos[1] - 1
+            local neighbors = getHexNeighbors(pos)
+            for i, dir in ipairs(neighbors) do
+                local newPos = pos + dir
+                self:add_target(newPos)
             end
-            temp_pos[2] = pos[2] - 1
-            self:add_target(temp_pos)
-            
-            if pos[2]%2 == 0 then
-                temp_pos[1] = pos[1] + 1
-            else
-                temp_pos[1] = pos[1] - 1
-            end
-            temp_pos[2] = pos[2] + 1
-            self:add_target(temp_pos)
         end
     end
 end
@@ -88,8 +56,7 @@ end
 function Tower:add_target(pos)
     local key = Tower.serialize_pos(pos)
     if self.targets[key] == nil then
-        self.targets[key] = {}
-        flower.table.copy(pos, self.targets[key])
+        self.targets[key] = vector(pos)
     end
 end
 
@@ -97,5 +64,3 @@ function Tower:upgrade(type)
     -- TODO: Change stats
     self.level = self.level + 1
 end
-
-return M
