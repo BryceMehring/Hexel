b4fd7f6a17d19c16378f2e08d9bd3d27fa56eb41
BryceMehring <brycemehring@gmail.com>
2014-11-13 00:59:34 -0600

    Added test script to check if debugging was enabled.

---
+++ scripts/tests/noDebugging.sh
@@ -0,0 +1,10 @@
+# Test to make sure that debugging is not enabled
+enableDebuggingCount=($(grep -r "enableDebugging()" ../../ | wc -l))
+
+if [ $enableDebuggingCount -gt 2 ]
+then
+  echo "Failed No Debugging Test"
+  exit 1
+fi
+
+echo "Passed No Debugging Test"
