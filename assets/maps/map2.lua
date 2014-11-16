eca4a38ac2afbd628104e43716ab4f7a7bf3a181
BryceMehring <brycemehring@gmail.com>
2014-11-16 16:42:55 -0600

    Using TOWER_TYPES to check for tower types.

--- assets/maps/map2.lua
+++ assets/maps/map2.lua
@@ -1,21 +1,12 @@
-
-local YELLOW = 1
-local RED = 2
-local GREEN = 3
-local BLUE = 4
-local EMPTY = 5
-local ENEMY = 6
-local VOID = 7
-
 local map = {}
 map.width = 10
 map.height = 35
 
 map.tiles = "assets/grid.sav"
 map.waves = {
-    {enemies = {RED, YELLOW, GREEN}, spawnRate = 0.5, length = 100},
-    {enemies = {RED, GREEN}, spawnRate = 0.4, length = 200},
-    {enemies = {RED}, spawnRate = 0.3, length = 300},
+    {enemies = {TOWER_TYPES.RED, TOWER_TYPES.YELLOW, TOWER_TYPES.GREEN}, spawnRate = 0.5, length = 100},
+    {enemies = {TOWER_TYPES.RED, TOWER_TYPES.GREEN}, spawnRate = 0.4, length = 200},
+    {enemies = {TOWER_TYPES.RED}, spawnRate = 0.3, length = 300},
 }
 
 return map
--- assets/towerTypes.lua
+++ assets/towerTypes.lua
@@ -1,6 +1,8 @@
 TOWER_TYPES = {
     ["YELLOW"] = 1,
+    ["SPAWN"] = 1,
     ["RED"] = 2,
+    ["TARGET"] = 2,
     ["GREEN"] = 3,
     ["BLUE"] = 4,
     ["EMPTY"] = 5,
