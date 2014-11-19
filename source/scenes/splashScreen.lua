321977cfce6018cdd19f5047a0e44dce110e789f
BryceMehring <brycemehring@gmail.com>
2014-11-19 00:30:04 -0600

    Cleaned up 07e4561893da88161fb5cc1c41f439fba5f18a11.

--- source/scenes/splashScreen.lua
+++ source/scenes/splashScreen.lua
@@ -37,15 +37,8 @@ end
 function onStart(e)
     flower.Executors.callLaterFrame(1, function()
         loadResources()
-        flower.openScene("source/scenes/mainMenu", {animation="fade"})
-        flower.closeScene()
+        flower.gotoScene("source/scenes/mainMenu", {animation="fade"})
     end)
-end
-
-function createEvent()
-    print("please")
-    loadResources()
-    flower.openScene("source/scenes/mainMenu")
 end
 
 function loadResources()
