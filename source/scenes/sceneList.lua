0592aeac155f27e62d864770669593ee6ce6e0a3
Austin Gantner <austingantner@gmail.com>
2014-12-01 20:48:21 -0600

    Added verses scene back

--- source/scenes/sceneList.lua
+++ source/scenes/sceneList.lua
@@ -6,6 +6,6 @@ local networkingScene = Configuration("Enable Networking") and 'source/scenes/mu
 return {
     {title = "Single Player",    scene = 'source/scenes/mapSelect',            openAnime = "fade", closeAnime = "fade"},
     {title = "Co-op",            scene = nil,                                  openAnime = "fade", closeAnime = "fade"},
-    {title = "Versus",           scene = networkingScene,                      openAnime = "fade", closeAnime = "fade"},
+    {title = "Versus",           scene = 'source/scenes/multiPlayer',                      openAnime = "fade", closeAnime = "fade"},
     {title = "Map Editor",       scene = "source/scenes/mapEditor",            openAnime = "fade", closeAnime = "fade"},
 }
