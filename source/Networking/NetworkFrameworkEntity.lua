ae7f11b67339adf08b567dd5d6c89be81f3424a0
Austin Gantner <austingantner@gmail.com>
2014-12-01 21:40:00 -0600

    Breaking change to versus, adding lua socket funct to nfe

--- source/Networking/NetworkFrameworkEntity.lua
+++ source/Networking/NetworkFrameworkEntity.lua
@@ -1,9 +1,23 @@
 local flower = flower
+-- LuaSocket should come with zerobrane
+local socket = require("socket")
 
 NetworkFrameworkEntity = flower.class()
 
 function NetworkFrameworkEntity:init(t)
-  self.MyIP = nil
-  self.TheirIP = nil
-  
+  --self.MyIP = nil
+  self.TheirIP = '192.168.1.21'
+  port = 8000
+  self.talkerConnection = assert(socket.connect(self.TheirIP, port))
+  s = assert(socket.bind(self.TheirIP, port))
+  self.listenerConnection = assert(s:accept())
+end
+
+function talker(text)
+	assert(self.talkerConnection:send(text .. "\n"))
+end
+
+function listener()
+  l, e = self.listenerConnection:receive()
+  return l
 end
